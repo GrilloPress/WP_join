f071ee67501b14ecd707e2f21d5dec457d95f186
Andrew Duckworth <grillopress@gmail.com>
2016-11-18 16:59:23 +0000

    add custom category template code and template part

--- category-meet-our-staff.php
+++ category-meet-our-staff.php
@@ -2,4 +2,41 @@
 /*
  *  Template to individually style the meet the staff cateogry
  */
-?>
+get_header(); ?>
+
+<section class="breadcrumb-container">
+  <div class="container">
+    <div class="col-md-12">
+      <?php sth_breadcrumbs(); ?>
+    </div>
+  </div>
+</section>
+
+<main id="primary" class="container" role="main">
+  <?php if ( have_posts() ) : ?>
+  <div class="col-md-12">
+    <header class="category-page-header">
+      <h1 class="category-page-title"><?php single_cat_title();?></h1>
+          <?php
+            the_archive_description( '<div class="lead taxonomy-description">', '</div>' );
+          ?>
+		</header><!-- .page-header -->
+  </div>
+    <?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					//
+					get_template_part( 'template-parts/content', 'category-meet-our-staff' );
+				?>
+
+    <?php endwhile; ?>
+
+  
+  <?php else :?>
+  
+    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+  
+  <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
---
+++ template-parts/content-category-meet-our-staff.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template used for displaying page content in the category-our-staff.php feed
+ *
+ * @package sth
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('category-article'); ?>>
+  <div class="col-md-3">
+  
+
+      <?php if ( has_post_thumbnail() ) :?>
+        <a href="<?php the_permalink() ;?>">
+          <?php the_post_thumbnail('medium', array('class' => 'img-responsive img-full')); ?>
+        </a>
+      <?php else :?>
+        <a href="<?php the_permalink() ;?>">
+          
+          <?php if ( in_category( 'patient-story' ) ) :?>
+            <img class="img-responsive img-full" src="<?php echo get_template_directory_uri() . "/images/hex_info_small.png"; ?>" alt="Patient Story">
+          <?php else :?>
+            <img class="img-responsive" src="<?php echo get_template_directory_uri() . "/images/news.jpg"; ?>" alt="News">
+          <?php endif ;?>
+          
+        </a>
+      <?php endif ;?>
+
+
+
+      <header class="entry-header">
+          <?php the_title( sprintf( '<h2 class="category-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+        </header><!-- .entry-header -->
+
+      <div class="entry-summary">
+          <?php the_excerpt(); ?>
+        </div><!-- .entry-summary -->
+
+        <footer class="entry-footer">
+        </footer><!-- .entry-footer -->
+
+
+  </div>
+</article><!-- #post-## -->
