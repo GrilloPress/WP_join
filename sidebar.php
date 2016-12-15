1541f4f501eaae5220dbb854d95e880379ad38f3
Andrew Duckworth <grillopress@gmail.com>
2016-12-15 16:56:26 +0000

    add page editor sidebar functionality

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,19 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <div id="secondary" class="widget-area" role="complementary">
   
+  <?php $PS_published = CFS()->get( 'ps_published' );
+  if ( $PS_published ):?>
+  
+  <div class="panel panel-primary panel-page-sidebar">
+    <div class="panel-heading">
+      <h3 class="panel-title"><?php echo CFS()->get( 'ps_title' );?></h3>
+    </div>
+    <div class="panel-body">
+      <?php echo CFS()->get( 'ps_body' );?>
+    </div>
+  </div>
+  <?php endif;?>
+  
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	<?php dynamic_sidebar( 'sidebar-2' ); ?>
   
